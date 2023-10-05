e64759637824f60566e3b10be12f9df988afd140
falakk123 <125361318+falakk123@users.noreply.github.com>
2023-10-05 11:47:31 -0700

    Add files via upload
    asssigment NO 5

---
+++ app.js
@@ -0,0 +1,33 @@
+alert("JS FILE HAS CORRECTLY ATTACHED FROM HTML FILE AND PROPERLY WORKING")
+
+
+// Practice exercise 1.4
+// Adding comments:
+// 1. Add a new statement to your JavaScript code by setting a variable value.  Since we will cover this in the next chapter, you can use the following line:
+// let a = 10;
+// 2. Add a comment at the end of the statement indicating that you set a value of  10.
+// 3. Print the value using console.log(). Add a comment explaining what this  will do.
+
+
+// Set a variable value
+var myVariable = "Hello, World!"
+
+let a = 10;
+console.log(a)
+
+//Add a comment explaining what this  will do..................
+//We use console.log() to print the value of myVariable to the console, and we added a comment explaining that this statement will log the value of myVariable to the console.
+
+
+// 4. At the end of your JavaScript code, use a multiple-line comment. In a real  production script, you might use this space to add a brief outline of the  purpose of the file. 
+/*
+  This JavaScript file is a simple example demonstrating variable assignment
+  and console logging. It sets a variable to the value 10 and then prints
+  the value to the console. This code serves as a basic illustration for
+  educational purposes.
+*/
+
+
+
+
+
---
+++ project.js
@@ -0,0 +1,37 @@
+// Chapter project 
+// Creating an HTML file and a linked JavaScript  file 
+// Create an HTML file and create a separate JavaScript file. Then, connect to the  JavaScript file from the HTML file. 
+// 1. In the JavaScript file, output your name into the console and add a multiple line comment to your code. 
+// 2. Try commenting out the console message in your JavaScript file so that  nothing shows in the console. 
+
+var MyName = "falak";
+
+
+// Commenting out the console message so that nothing shows in the console
+// console.log("My name is: " + myName);
+
+
+// Self-check quiz
+// 1. What is the HTML syntax to add an external JavaScript file?
+
+<script src="./project.js"></script>
+// <script>: This tag is used to include JavaScript code in your HTML document.
+//  src: This attribute specifies the source (URL) of the external JavaScript file. You should replace "path-to-your-script.js" with the actual path or URL to your JavaScript file. */}
+
+
+
+//2. Can you run JavaScript in a file with a JS extension in your browser?
+//Yes, you can run JavaScript code stored in a file with a.js extension in your browser.To do so, you can include the JavaScript file in an HTML document using a <script> tag, and then open the HTML file in your browser.
+
+
+//3. How do you write a multiple-line comment in JavaScript?
+/*
+  This is a multi-line comment in JavaScript.
+  You can write as many lines of comments as you need.
+  This is multiple Line Comment
+*/
+//console.log("This is single line comment.");
+
+
+// 4. What is the best way to remove a line of code from running that you might  want to keep as you debug?
+//Comment Out its best way to remove a line of code from running that you might  want to keep as you debug
